d7b5f684502d22bb64e6eed74f93a6a0ba3a87e9
architechto <cristian.contreras1@gmail.com>
2018-04-15 16:35:57 -0400

    devices gem and custom routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  get 'pages/home'
 
-  get 'pages/about'
+  root to: "pages#home"
 
-  get 'pages/contact'
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' 
 
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
