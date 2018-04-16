6113d4c6c4345bb33330357cfc5337105e14d5e0
architechto <cristian.contreras1@gmail.com>
2018-04-15 23:00:05 -0400

    custom routes for auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  root to: "pages#home"
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'} 
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
@@ -10,5 +9,7 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' 
 
   resources :blogs
+
+  root to: "pages#home"
 
 end
