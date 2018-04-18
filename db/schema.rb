1301b3bfa3d93e7da1469acf9bc53f0c9718417c
architechto <cristian.contreras1@gmail.com>
2018-04-18 00:56:15 -0230

    integrated topic to blog relations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180415204245) do
+ActiveRecord::Schema.define(version: 20180418031939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20180415204245) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "topic_id"
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "portfolios", force: :cascade do |t|
@@ -35,6 +37,12 @@ ActiveRecord::Schema.define(version: 20180415204245) do
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_utilized"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -57,4 +65,5 @@ ActiveRecord::Schema.define(version: 20180415204245) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "blogs", "topics"
 end
