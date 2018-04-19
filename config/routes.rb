0712bd3fff392d0a6c1aac39001f73c6a2a33108
architechto <cristian.contreras1@gmail.com>
2018-04-18 23:32:20 -0230

    custom scope

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
 
   resources :portfolios, except: [:show]
+  get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' 
 
   resources :blogs
